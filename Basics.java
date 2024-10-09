a7811f5006daf5808caf7c34456ae7ae8bb6ab96
Pavan-personal <kumarpavan39877@gmail.com>
2024-10-09 22:42:01 +0530

    subsets

---
+++ Basics.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Basics {
+
+    public static void printSubsetsHelper(int[] arr,ArrayList<Integer> temp,int i){
+        if(i >= arr.length){
+            System.out.println(temp);
+            return;
+        }
+        List<List<Integer>> l = new ArrayList<>();
+        l.add
+        temp.add(arr[i]);
+        printSubsetsHelper(arr,temp,i+1);
+        temp.remove(temp.size() - 1);
+        printSubsetsHelper(arr,temp,i+1);
+    }
+
+    public static void printSubsets(int[] a){
+        ArrayList<Integer> temp = new ArrayList<>();
+        printSubsetsHelper(a,temp,0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int[] arr = {1,2,3};
+        printSubsets(arr);
+
+    }
+}
